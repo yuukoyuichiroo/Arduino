aae127ba7f607c519d224ab12f0650bf0ca9a852
YuukoYuichiroo <35053287+yuukoyuichiroo@users.noreply.github.com>
2023-10-02 20:18:16 +0700

    create main.cpp
    adding some hello world program

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Output "Hello World!" to the console
+    std::cout << "Hello World!";
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
